924b3e6c0117a0123849aaf0f201aef4deef24d9
tkaburagi <t.kaburagi@me.com>
2022-10-26 17:45:18 +0900

    first commit

---
+++ main.tf
@@ -0,0 +1,34 @@
+terraform {
+
+}
+
+provider "azurerm" {
+	features {}
+}
+
+resource "azurerm_resource_group" "example" {
+	name     = "example-resources"
+	location = "West Europe"
+}
+
+resource "azurerm_app_service_plan" "example" {
+	name                = "example-appserviceplan"
+	location            = azurerm_resource_group.example.location
+	resource_group_name = azurerm_resource_group.example.name
+
+	sku {
+		tier = "Standard"
+		size = "S1"
+	}
+}
+
+resource "azurerm_app_service" "my-app-service" {
+	app_service_plan_id = azurerm_app_service_plan.example.id
+	location            = azurerm_resource_group.example.location
+	name                = "my-app-tkabu-service"
+	resource_group_name = azurerm_resource_group.example.name
+
+	app_settings = {
+		"WEBSITE_HEALTHCHECK_MAXPINGFAILURES" = 10
+	}
+}
