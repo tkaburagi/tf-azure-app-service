bf4fb3997317268399bb16dde837d66bdce5d32f
Kabu <t.kaburagi@me.com>
2022-11-04 14:24:23 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -29,6 +29,6 @@ resource "azurerm_app_service" "my-app-service" {
 	resource_group_name = azurerm_resource_group.example.name
 
 	app_settings = {
-		"WEBSITE_HEALTHCHECK_MAXPINGFAILURES" = 10
+		"WEBSITE_HEALTHCHECK_MAXPINGFAILURES" = 18
 	}
 }
